70dffa8b93ee12e527f4cf37170991eaacbac8f6
Dane Petersen <thegreatsunra@gmail.com>
2022-05-29 20:32:41 -0500

    Enable and require MQTT username and password

--- mosquitto/docker-entrypoint.sh
+++ mosquitto/docker-entrypoint.sh
@@ -2,4 +2,14 @@
 
 set -euo pipefail
 
+
+if [ -z "${MOSQUITTO_USERNAME}" ] || [ -z "${MOSQUITTO_PASSWORD}" ]; then
+  echo MOSQUITTO_USERNAME or MOSQUITTO_PASSWORD not defined
+  exit 1
+fi
+
+## create mosquitto passwordfile
+touch passwordfile
+mosquitto_passwd -b passwordfile "$MOSQUITTO_USERNAME" "$MOSQUITTO_PASSWORD"
+
 exec "$@"
